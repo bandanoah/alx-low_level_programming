12e7c5b166bb50ca447f3587145245bd0a605363
bandanoah <bandahnoah007@gmail.com>
2024-04-25 16:29:23 +0300

    lowercase & uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void){
+
+    char lower= 'a';
+    char upper= 'A';
+
+    while (lower <= 'z'){
+
+        putchar(lower);
+        lower++;
+    }
+    while (upper <= 'Z'){
+
+        putchar(upper);
+        upper++;
+    }
+
+    return 0;
+}
