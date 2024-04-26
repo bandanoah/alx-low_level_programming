c6c527b88807d3210f226df1672a673d9c0bde4c
bandanoah <bandahnoah007@gmail.com>
2024-04-26 12:07:43 +0300

    hexadeciamal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+
+    int n;
+
+    char l;
+
+    for (n = 0; n < 10; n++){
+
+        putchar('0' + n);
+    }
+
+    for (l = 'a'; l <= 'f'; l++){
+
+        putchar(l);
+    }
+
+    putchar('\n');
+
+    return 0;
+
+
+}
