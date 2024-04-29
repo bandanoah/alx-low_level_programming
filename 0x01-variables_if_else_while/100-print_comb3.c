3fc036f57a63ab5e0a0a65ee5638b925a426a64f
bandanoah <bandahnoah007@gmail.com>
2024-04-29 07:50:50 +0300

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (void){
+
+    int i,j;
+     for (i = 0; i <= 9; i++){
+
+        for (j = 1; j <= 8; j++){
+
+            putchar('0' + i);
+            putchar('0' + j);
+
+            if ( i !=9 || j !=8){
+
+                putchar(',');
+                putchar(' ');
+            }
+        }
+     }
+
+     putchar('\n');
+
+     return 0;
+
+}
