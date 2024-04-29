9249ef73162c749b519d765f1043f1208ba07947
bandanoah <bandahnoah007@gmail.com>
2024-04-29 07:51:16 +0300

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (void){
+
+    int i,j,k;
+
+    for (i =0; i <= 9; i++){
+
+        for (j = 1; j <=8; j++){
+
+            for (k = 2; k <= 7; k++){
+
+                putchar('0' + i);
+                putchar('0' + j);
+                putchar('0' + k);
+
+                if( i != 9 || k != 7){
+
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0; 
+
+}
