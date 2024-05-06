5c045fbcb1d7c09cfa4a4f813f1e11174f4aadb1
bandanoah <bandahnoah007@gmail.com>
2024-05-06 07:57:09 +0300

    times table

---
+++ 0x02-functions_nested_loops/times_table.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void times_table(void);
+
+void times_table(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar((k / 10) + '0');
+				putchar((k % 10) + '0');
+			}
+		}
+		putchar('\n');
+	}
+}
+
+int main (void){
+
+    times_table();
+
+    return 0;
+}
