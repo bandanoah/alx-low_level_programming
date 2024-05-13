446f96e19db0d51698864f846f322b31f55e0a5b
bandanoah <bandahnoah007@gmail.com>
2024-05-13 11:53:47 +0300

    strcat modified

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,11 +5,11 @@ char *_strcat(char *dest, char *src);
 char *_strcat(char *dest, char *src){
     int i = 0;
     int d = 0;
-    while (i != '\0'){
+    while (dest[i] != '\0'){
         dest[i];
         i++;
     }
-    while (d != '\0')
+    while (src[d] != '\0')
     {
         dest[i] = src[d];
         i++;
@@ -20,7 +20,7 @@ char *_strcat(char *dest, char *src){
 }
 int main (void){
     char d[50] = "Hello";
-    char s[20] = "world";
+    char s[20] = " world! \n";
 
     char *f=_strcat(d,s);
     printf("%s \n",f);
