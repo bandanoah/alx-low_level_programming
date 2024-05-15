68843936534e77c79030e8042c853142313a88b8
bandanoah <bandahnoah007@gmail.com>
2024-05-15 13:14:12 +0300

    capitalization

---
+++ 0x06-pointers_arrays_strings/6-cap.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
+int main (void){
+    char str[]="Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    
+    char *p = cap_string(str);
+    printf("%s",str);
+    printf("%s",p);
+
+    return 0;
+}
