e10d97169b96e98fd8e8aded2b8b1a0f679658d4
bandanoah <bandahnoah007@gmail.com>
2024-05-17 11:14:42 +0300

    understanding pointers

---
+++ 0x06-pointers_arrays_strings/pointers_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void){
+
+    int a , b , c;
+    a = b = c ;
+
+    printf("Enter 3 num: ");
+    scanf("%d %d %d", &a,&b,&c);
+    printf("Result:%d\n", a+b+c);
+
+}
