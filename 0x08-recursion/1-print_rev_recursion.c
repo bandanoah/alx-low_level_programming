bad9b8ecb298fd0d0e5c5c8995b177f9ab17ab48
bandanoah <bandahnoah007@gmail.com>
2024-06-07 13:39:11 +0300

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void _print_rev_recursion(char *s );
+
+void _print_rev_recursion(char *s)
+{
+    
+    //create a base case to terminate 
+    //recursive function that calls itself
+    if (*s == '\0')
+    {
+        return;
+    }
+    
+    _print_rev_recursion(s + 1);
+    putchar(*s);
+     
+}
+int main (void)
+{
+    char str[] = "Hello World";
+    _print_rev_recursion(str);
+    putchar('\n');
+    return 0;
+}
