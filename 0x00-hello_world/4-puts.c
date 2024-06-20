a473017a8f86413a99ccce95e82cd2cf55862eab
bandanoah <bandanoah007@gmail.com>
2024-06-20 11:37:11 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -15,10 +15,11 @@
  */
 int main(void)
 {
-	const char *message = "Programming is like building a multilingual puzzle.";
+	const char *message = "Programming is like building a multilingual puzzle";
 
 	/* Print the message using puts */
 	puts(message);
 
 	return (0);
 }
+
