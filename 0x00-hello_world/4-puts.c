6dd05c6a674e2786ead917b61ea956a5f656a013
bandanoah <bandanoah007@gmail.com>
2024-06-20 10:57:29 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
-int main () {
+/*
+ * File:   4-puts.c
+ * Author: [Your Name]
+ *
+ * This program prints a specific string using the puts function.
+ * It demonstrates how to use puts to output a string to the standard output.
+ */
 
-	const char *message = "Programming is like building a multilingual puzzle.";
-	puts (message);
-	return 0;
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    const char *message = "Programming is like building a multilingual puzzle.";
+
+    /* Print the message using puts */
+    puts(message);
+
+    return (0);
 }
+
