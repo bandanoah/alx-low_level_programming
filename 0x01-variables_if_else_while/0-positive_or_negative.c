83b2f778af791c059ecb5bd24a7e509fc902abdd
bandanoah <bandahnoah007@gmail.com>
2024-06-21 12:30:59 +0300

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,18 +11,21 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-    if (n > 0){
+    if (n > 0)
+    {
         
        const char *p = "is positive";
         
         printf("%i %s\n",n , p);
-    }else if (n == 0) {
+    }else if (n == 0) 
+    {
 
        const char *z = " is zero";
 
         printf("%i %s\n", n , z);
     }
-    else if(n < 0) { 
+    else if(n < 0) 
+    { 
         const char *g = " is negative";
 
         printf("%i %s\n", n ,g);
