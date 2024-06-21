e77b3afcbf55545d6d42db7067b22dce95044bfb
bandanoah <bandahnoah007@gmail.com>
2024-06-21 12:48:54 +0300

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
-int main(void){
-
+int main(void)
+{
     int n;
-
     char l;
 
-    for (n = 0; n < 10; n++){
+    for (n = 0; n < 10; n++)
+    {
 
         putchar('0' + n);
     }
-
-    for (l = 'a'; l <= 'f'; l++){
+    for (l = 'a'; l <= 'f'; l++)
+    {
 
         putchar(l);
     }
-
     putchar('\n');
-
     return 0;
-
-
 }
