d51fc72e4ecb66ec219b0537c4e7985943d2fec5
bandanoah <bandahnoah007@gmail.com>
2024-06-21 12:47:13 +0300

    num

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-int main (void){
-
-
+int main (void)
+{
     int b;
 
-    for (b =0; b < 10; b++){
+    for (b =0; b < 10; b++)
+    {
 
         putchar('0' + b);
     }
     putchar('\n');
-
     return 0;
 }
