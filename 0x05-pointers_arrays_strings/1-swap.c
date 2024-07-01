caf23eb6d80fa586253079f67a81d32aa0ed93db
bandanoah <bandanoah007@gmail.com>
2024-07-01 04:59:24 -0700

    swaps two num

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /*
  * swap_int - swaps the values of two intergers
  * @a:input
@@ -8,6 +9,9 @@
 
 void swap_int(int *a, int *b)
 {
-    *a = 402;
-    *b = 98;
+    int m;
+
+    m = *a;
+    *a = *b;
+    *b = m;
 }
