bd489404ee2e5b9d82917b701192cfd43c3a2004
bandanoah <bandahnoah007@gmail.com>
2024-07-04 13:00:33 +0300

    concat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,38 +1,29 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
 
-char *_strncat(char *dest, char *src, int n);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-char *_strncat(char *dest, char *src, int n){
-
-    int i = 0;
-    int d = 0;
-
-    while (dest[i] != '\0'){
-        dest[i];
-        i++;
-    }
-
-    int t =strlen(src);
-    n = 1024;
-    while (t <= n && src[d] != '\0'){
-        dest[i] = src[d];
-        i++;
-        d++;
-    }
-    dest[i] = '\0';
-
-    return dest;
-
-}
-
-int main (void){
-    char s1 [98] = "Hello ";
-    char s2 [] = "World!\n";
-
-    char *res;
-    res = _strncat(s1,s2,1024);
-    printf("%s",res);
-
-    return 0;
+	dest[i] = '\0';
+	return (dest);
 }
