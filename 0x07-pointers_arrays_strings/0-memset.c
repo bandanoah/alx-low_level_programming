e265532f0f4c729c7c58c6916047688d3f526857
bandanoah <bandanoah007@gmail.com>
2024-07-08 10:12:38 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,10 +15,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
     int i = 0;
 
-    for (;n > 0; i++)
-    {
-        s[i] = b;
-	n--;
-    }
+    while(n > 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
     return (s);
 }
