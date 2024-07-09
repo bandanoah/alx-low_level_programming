e5eb9d4914039df7df0de229493bbcfefa0bd67e
bandanoah <bandahnoah007@gmail.com>
2024-07-09 13:16:43 +0300

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,8 +11,9 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
     int i = 0;
+    int k = n;
 
-    for (; i < n; i++)
+    for (; i < k; i++)
     {
         dest[i] = src[i];
         n--;
