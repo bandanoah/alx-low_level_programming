271c364301a4e9d2de50764f5449465b42a6dc52
bandanoah <bandahnoah007@gmail.com>
2024-07-16 15:12:43 +0300

    update

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Check for lowercase character
+ * @c: Character to be checked
+ * Return: 1 for lowecase character or 0 for anything else
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+
+}
