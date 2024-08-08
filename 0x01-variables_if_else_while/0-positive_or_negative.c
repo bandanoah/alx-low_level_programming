9ff3f5df6ec6131b98ac200a584e6a10996c6394
bandanoah <141237395+bandanoah@users.noreply.github.com>
2024-08-08 09:12:03 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,38 +5,33 @@
 /**
  * main - Entry point of the program
  * 
- * Description: Generates random numbers and prints out if the number
- * 		is positive, negative or zero
+ * Description: Generates a random number and prints whether the number
+ *              is positive, negative, or zero.
  * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
     if (n > 0)
     {
-        
-       const char *p = "is positive";
-        
-        printf("%i %s\n",n , p);
-    }else if (n == 0) 
+        const char *p = "is positive";
+        printf("%d %s\n", n, p);
+    }
+    else if (n == 0)
     {
-
-       const char *z = " is zero";
-
-        printf("%i %s\n", n , z);
+        const char *z = "is zero";
+        printf("%d %s\n", n, z);
     }
-    else if(n < 0) 
-    { 
-        const char *g = " is negative";
-
-        printf("%i %s\n", n ,g);
+    else
+    {
+        const char *g = "is negative";
+        printf("%d %s\n", n, g);
     }
 
-	
-	return (0);
+    return (0);
 }
