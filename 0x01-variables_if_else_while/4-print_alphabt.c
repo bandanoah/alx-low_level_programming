32b722870376c6a82627827aa22315c817d691da
bandanoah <141237395+bandanoah@users.noreply.github.com>
2024-08-09 14:48:37 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,17 +4,16 @@
  * Description: Prints out lowercase alphabet except q and e
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-    char l;
-
-    for (l = 'a'; l <= 'z'; l++)
-    {
-        if (l != 'q' && l !='e')
-        {
-            putchar(l); 
-        }
-    }
-    putchar('\n');
-    return 0;
+char l;
+for (l = 'a'; l <= 'z'; l++)
+{
+if (l != 'q' && l != 'e')
+{
+putchar(l);
+}
+}
+putchar('\n');
+return (0);
 }
